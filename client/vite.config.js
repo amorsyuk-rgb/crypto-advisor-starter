a784a87fdf446a7f3fc52dc4ca7d2c67f2457cb5
amorsyuk-rgb <amorsyuk@gmail.com>
2025-10-30 03:30:14 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
-  root: ".",                          // Root is client/
   plugins: [react()],
   build: {
     outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(__dirname, "index.html"), // entry point
-    },
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"), // optional cleaner imports
-    },
   },
 });
