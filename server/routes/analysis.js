4f2c8da79d3001fdc53a85210594590a5c9946e3
amorsyuk-rgb <amorsyuk@gmail.com>
2025-10-31 18:22:12 +0200

    Update analysis.js

--- server/routes/analysis.js
+++ server/routes/analysis.js
@@ -2,3 +2,4 @@ router.get("/:symbol/analysis", async (req, res) => {
   const { symbol } = req.params;
   res.json({ symbol, price: 68000, indicators: { ema50: 67500, ema200: 66000 } });
 });
+export default router;
