2aa7cb17c356e335ec8078d488cd1d4cf4e3ab3b
amorsyuk-rgb <amorsyuk@gmail.com>
2025-10-31 20:26:10 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import analysisRoutes from "./routes/analysis.js";
+import assetsRoutes from "./routes/assets.js";
 
+app.use("/api/assets", assetsRoutes);
 const app = express();
 
 app.use("/api/analysis", analysisRoutes); // ✅ mount router
